a0a132aa7fff9910dcd8126ce0f8fb8630d013e3
Andrew MacLean <andrewmaclean@dreambook.lan>
2019-06-25 18:40:16 -0700

    build file

---
+++ client/build/precache-manifest.3cbaf4f03d2854ba87e9de294980ce47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7011c0c4146477daa81cbce9ccbf2de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d83a9cecf011e4ba2fa",
+    "url": "/static/css/main.dbc0ff19.chunk.css"
+  },
+  {
+    "revision": "4894903e464fd7b5f1d6",
+    "url": "/static/js/2.5ab7f409.chunk.js"
+  },
+  {
+    "revision": "1d83a9cecf011e4ba2fa",
+    "url": "/static/js/main.c0e2c9db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
